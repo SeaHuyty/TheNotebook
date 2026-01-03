ce0252b876c0849f323f817aeb53bc5b77828c62
SeaHuyty <seahuyty4444@gmail.com>
2026-01-03 16:17:47 +0700

    Create: Structure for Setting Page

--- lib/shared/widgets/app_drawer.dart
+++ lib/shared/widgets/app_drawer.dart
@@ -3,6 +3,7 @@ import 'package:the_notebook/features/diary/data/repositories/diary_repository.d
 import 'package:the_notebook/features/diary/data/repositories/task_repository.dart';
 import 'package:the_notebook/features/notebook/data/repository/notebook_repository.dart';
 import 'package:the_notebook/features/notebook/presentation/notebook_page.dart';
+import 'package:the_notebook/features/setting/presentation/pages/setting.dart';
 
 class AppDrawer extends StatelessWidget {
   final DiaryRepository? diaryRepo;
@@ -30,6 +31,7 @@ class AppDrawer extends StatelessWidget {
             child: const Text('Hello Tengyi', style: TextStyle(fontSize: 22)),
           ),
           ListTile(
+            leading: Icon(Icons.book_outlined),
             title: const Text('Notebook'),
             selected: currentPage == 'notebook',
             onTap: () {
@@ -48,7 +50,38 @@ class AppDrawer extends StatelessWidget {
               }
             },
           ),
-          ListTile(title: const Text('Setting')),
+          ListTile(
+            leading: Icon(Icons.settings_outlined),
+            title: const Text('Setting'),
+            selected: currentPage == 'setting',
+            onTap: () {
+              if (currentPage != 'setting') {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => SettingPage(
+                            notebookRepo: notebookRepo,
+                            diaryRepo: diaryRepo,
+                            taskRepo: taskRepo,
+                          )),
+                );
+              } else {
+                Navigator.pop(context);
+              }
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.group_outlined),
+            title: Text('Share with friends'),
+          ),
+          ListTile(
+            leading: Icon(Icons.star_border_rounded),
+            title: Text('Rate the app'),
+          ),
+          ListTile(
+            leading: Icon(Icons.contact_support_outlined),
+            title: Text('Contact the support team'),
+          ),
         ],
       ),
     );
